6d46a2dded15353a1976163b2410d7dc0e43e39a
Gabin Desserprit <desserprit.gabin@gmail.com>
2022-06-16 10:13:57 +0200

    Experimental Custom Playwright Browser works with Crawlee

--- packages/custom--crawler--playwright/src/index.ts
+++ packages/custom--crawler--playwright/src/index.ts
@@ -1,16 +1,23 @@
+import { DomHandler } from 'htmlparser2';
+import { WritableStream } from 'htmlparser2/lib/WritableStream';
 import { addTimeoutToPromise, tryCancel } from '@apify/timeout';
+import { concatStreamToBuffer, readStreamToString } from '@apify/utilities';
 import { BrowserCrawler } from '@crawlee/browser';
 import { BrowserPlugin, CommonPage } from '@crawlee/browser-pool';
 import { Request } from '@crawlee/core';
 import { CrawlingContext, EnqueueLinksOptions, mergeCookies, PlaywrightCookie, PlaywrightCrawlerOptions, PlaywrightCrawlingContext, PlaywrightLaunchContext, PlaywrightLauncher, Session } from '@crawlee/playwright';
 import { Dictionary } from '@crawlee/types';
-import { CheerioRoot } from '@crawlee/utils';
+import { CheerioRoot, parseContentTypeFromResponse } from '@crawlee/utils';
 import * as FT from '@usefelps/types';
 import { gotScraping, Method, OptionsInit, Request as GotRequest, TimeoutError } from 'got-scraping';
 import type { IncomingMessage } from 'http';
 import getPath from 'lodash.get';
 import { chromium, firefox, webkit } from 'playwright';
 import { Cookie } from 'tough-cookie';
+import iconv from 'iconv-lite';
+import util from 'util';
+import * as cheerio from 'cheerio';
+import { IncomingHttpHeaders } from 'http';
 
 export interface RequestFunctionOptions {
     request: Request;
@@ -28,6 +35,9 @@ export interface CheerioCrawlingContext extends CrawlingContext<Dictionary> {
     contentType: { type: string; encoding: string };
     response: IncomingMessage;
 }
+
+const HTML_AND_XML_MIME_TYPES = ['text/html', 'text/xml', 'application/xhtml+xml', 'application/xml'];
+const APPLICATION_JSON_MIME_TYPE = 'application/json';
 
 const getBrowserPlugin = (browser: string, launchContext: PlaywrightLaunchContext): BrowserPlugin => {
     let launcher;
@@ -60,7 +70,6 @@ export const CONST = {
 
 export default class CustomPlaywrightCrawler extends BrowserCrawler {
     protected browserPlugins: { [browser: string]: BrowserPlugin } = {};
-    protected requestTimeoutMillis: number;
     protected ignoreSslErrors: boolean;
     // events: EventEmitter
 
@@ -90,7 +99,7 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
 
 
     protected override async _applyCookies({ session, request, page }: PlaywrightCrawlingContext, preHooksCookies: string, postHooksCookies: string, gotOptions?: OptionsInit) {
-        console.log('_applyCookies');
+        // console.log('_applyCookies');
 
         if (page) {
             const sessionCookie = session?.getPuppeteerCookies(request.url) ?? [];
@@ -116,13 +125,13 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
     }
 
     protected _getRequestOptions(request: Request, session?: Session, proxyUrl?: string, gotOptions?: OptionsInit) {
-        console.log('_getRequestOptions');
+        // console.log('_getRequestOptions');
 
         const requestOptions: OptionsInit & { isStream: true } = {
             url: request.url,
             method: request.method as Method,
             proxyUrl,
-            timeout: { request: this.requestTimeoutMillis },
+            timeout: { request: this.requestHandlerTimeoutMillis },
             sessionToken: session,
             ...gotOptions,
             headers: { ...request.headers, ...gotOptions?.headers },
@@ -146,7 +155,7 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
     }
 
     private _requestAsBrowser = (options: OptionsInit & { isStream: true }) => {
-        console.log('_requestAsBrowser');
+        // console.log('_requestAsBrowser');
 
         return new Promise<IncomingMessage>((resolve, reject) => {
             const stream = gotScraping(options);
@@ -168,13 +177,13 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
         const crawlerOptions = getPath(context?.request?.userData || {}, CONST.CRAWLER_OPTIONS) as CustomPlaywrightCrawlerOptions;
         const { mode = 'http' } = crawlerOptions || {};
 
-        console.log('_navigationHandler', { mode });
+        // console.log('_navigationHandler', { mode });
 
         if (mode === 'http') {
             const { session, request, proxyUrl, gotOptions } = context as unknown as RequestFunctionOptions;
             const opts = this._getRequestOptions(request, session, proxyUrl, gotOptions);
 
-            await addTimeoutToPromise(
+            return addTimeoutToPromise(
                 async () => {
                     try {
                         return await this._requestAsBrowser(opts);
@@ -187,8 +196,8 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
                         throw e;
                     }
                 },
-                this.requestTimeoutMillis,
-                `request timed out after ${this.requestTimeoutMillis / 1000} seconds.`,
+                this.requestHandlerTimeoutMillis,
+                `request timed out after ${this.requestHandlerTimeoutMillis / 1000} seconds.`,
             );
         }
 
@@ -196,9 +205,8 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
     }
 
 
-    override async _handleNavigation(crawlingContext: FT.RequestContext) {
-        console.log('_handleNavigation');
-        console.log(crawlingContext);
+    override async _handleNavigation(crawlingContext: FT.ReallyAny) {
+        // console.log('_handleNavigation');
 
         if ('page' in crawlingContext) {
             const gotoOptions = { timeout: this.navigationTimeoutMillis } as Dictionary;
@@ -245,8 +253,136 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
         }
     }
 
+    protected async _parseHtmlToDom(response: IncomingMessage) {
+        return new Promise((resolve, reject) => {
+            const domHandler = new DomHandler((err, dom) => {
+                if (err) reject(err);
+                else resolve(dom);
+            });
+            const parser = new WritableStream(domHandler, { decodeEntities: true });
+            parser.on('error', reject);
+            response
+                .on('error', reject)
+                .pipe(parser);
+        });
+    }
+
+    protected async _parseResponse(request: Request, responseStream: IncomingMessage) {
+        const { statusCode } = responseStream;
+        const { type, charset } = parseContentTypeFromResponse(responseStream);
+        const { response, encoding } = this._encodeResponse(request, responseStream, charset);
+        const contentType = { type, encoding };
+
+        if (statusCode! >= 500) {
+            const body = await readStreamToString(response, encoding);
+
+            // Errors are often sent as JSON, so attempt to parse them,
+            // despite Accept header being set to text/html.
+            if (type === APPLICATION_JSON_MIME_TYPE) {
+                const errorResponse = JSON.parse(body);
+                let { message } = errorResponse;
+                if (!message) message = util.inspect(errorResponse, { depth: 1, maxArrayLength: 10 });
+                throw new Error(`${statusCode} - ${message}`);
+            }
+
+            // It's not a JSON so it's probably some text. Get the first 100 chars of it.
+            throw new Error(`${statusCode} - Internal Server Error: ${body.substr(0, 100)}`);
+        } else if (HTML_AND_XML_MIME_TYPES.includes(type)) {
+            const dom = await this._parseHtmlToDom(response);
+            return ({ dom, isXml: type.includes('xml'), response, contentType });
+        } else {
+            const body = await concatStreamToBuffer(response);
+            return { body, response, contentType };
+        }
+    }
+
+    protected _encodeResponse(request: Request, response: IncomingMessage, encoding: BufferEncoding): {
+        encoding: BufferEncoding;
+        response: IncomingMessage;
+    } {
+        // Fall back to utf-8 if we still don't have encoding.
+        const utf8 = 'utf8';
+        if (!encoding) return { response, encoding: utf8 };
+
+        // This means that the encoding is one of Node.js supported
+        // encodings and we don't need to re-encode it.
+        if (Buffer.isEncoding(encoding)) return { response, encoding };
+
+        // Try to re-encode a variety of unsupported encodings to utf-8
+        if (iconv.encodingExists(encoding)) {
+            const encodeStream = iconv.encodeStream(utf8);
+            const decodeStream = iconv.decodeStream(encoding).on('error', (err) => encodeStream.emit('error', err));
+            response.on('error', (err: Error) => decodeStream.emit('error', err));
+            const encodedResponse = response.pipe(decodeStream).pipe(encodeStream) as NodeJS.ReadWriteStream & {
+                statusCode?: number;
+                headers: IncomingHttpHeaders;
+                url?: string;
+            };
+            encodedResponse.statusCode = response.statusCode;
+            encodedResponse.headers = response.headers;
+            encodedResponse.url = response.url;
+            return {
+                response: encodedResponse as any,
+                encoding: utf8,
+            };
+        }
+
+        throw new Error(`Resource ${request.url} served with unsupported charset/encoding: ${encoding}`);
+    }
+
     override async _responseHandler(crawlingContext: FT.ReallyAny): Promise<void> {
         const { response, session, request, page } = crawlingContext;
+
+        if (!page) {
+            const { dom, isXml, body, contentType, response } = await this._parseResponse(request, crawlingContext.response!);
+            tryCancel();
+
+            if (this.useSessionPool) {
+                this._throwOnBlockedRequest(session!, response.statusCode!);
+            }
+
+            if (this.persistCookiesPerSession) {
+                session!.setCookiesFromResponse(response);
+            }
+
+            const $ = dom
+                ? cheerio.load(dom as string, {
+                    xmlMode: isXml,
+                    // Recent versions of cheerio use parse5 as the HTML parser/serializer. It's more strict than htmlparser2
+                    // and not good for scraping. It also does not have a great streaming interface.
+                    // Here we tell cheerio to use htmlparser2 for serialization, otherwise the conflict produces weird errors.
+                    _useHtmlParser2: true,
+                } as FT.ReallyAny) // CheerioOptions
+                : null;
+
+            crawlingContext.$ = $!;
+            crawlingContext.contentType = contentType;
+            crawlingContext.response = response;
+
+            Object.defineProperty(crawlingContext, 'json', {
+                get() {
+                    if (contentType.type !== APPLICATION_JSON_MIME_TYPE) return null;
+                    const jsonString = body!.toString(contentType.encoding);
+                    return JSON.parse(jsonString);
+                },
+            });
+
+            Object.defineProperty(crawlingContext, 'body', {
+                get() {
+                    // NOTE: For XML/HTML documents, we don't store the original body and only reconstruct it from Cheerio's DOM.
+                    // This is to save memory for high-concurrency crawls. The downside is that changes
+                    // made to DOM are reflected in the HTML, but we can live with that...
+                    if (dom) {
+                        return isXml ? $!.xml() : $!.html({ decodeEntities: false });
+                    }
+                    return body;
+                },
+            });
+        }
+
+        if(page) {
+            crawlingContext.body = await page.content();
+        }
 
         if (this.sessionPool && response && session) {
             if (typeof response === 'object' && typeof response.status === 'function') {
@@ -260,9 +396,8 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
         else request.loadedUrl = request.url;
     }
 
-
-    override async _runRequestHandler(context: PlaywrightCrawlingContext) {
-        console.log('_runRequestHandler');
+    override async _runRequestHandler(context: FT.RequestContext) {
+        // console.log('_runRequestHandler');
 
         // registerUtilsToContext(context);
 
@@ -321,6 +456,8 @@ export default class CustomPlaywrightCrawler extends BrowserCrawler {
                         const cookies = await context.browserController.getCookies(page);
                         tryCancel();
                         session?.setPuppeteerCookies(cookies, request.loadedUrl!);
+                    } else {
+                        session!.setCookiesFromResponse(context.response);
                     }
                 }
             }
--- packages/test-run/src/index.ts
+++ packages/test-run/src/index.ts
@@ -1,21 +1,41 @@
 import { Actor } from 'apify';
 import { RequestQueue } from '@crawlee/core';
 import CustomPlaywrightCrawler from '@usefelps/custom--crawler--playwright';
+import * as FT from '@usefelps/types';
 
 (async () => {
     await Actor.init();
 
     const requestQueue = await RequestQueue.open();
-    await requestQueue.addRequest({
-        url: 'https://www.apify.com', userData: {
-            // __crawlingOptions: { mode: 'firefox' }
+
+    await requestQueue.addRequests([
+        {
+            url: 'https://www.apify.com',
+            uniqueKey: '1',
+            userData: {
+                __crawlerOptions: { mode: 'http' }
+            }
+        },
+        {
+            url: 'https://www.apify.com',
+            uniqueKey: '2',
+            userData: {
+                __crawlerOptions: { mode: 'chromium' }
+            }
+        },
+        {
+            url: 'https://www.apify.com',
+            uniqueKey: '3',
+            userData: {
+                __crawlerOptions: { mode: 'firefox' }
+            }
         }
-    });
+    ]);
 
     const crawler = new CustomPlaywrightCrawler({
         requestQueue,
         async requestHandler({ request }) {
-            console.log(request);
+            console.log(`Processing request via ${(request.userData as FT.ReallyAny).__crawlerOptions.mode} mode`);
         },
         async failedRequestHandler(inputs) {
             console.log(inputs);
